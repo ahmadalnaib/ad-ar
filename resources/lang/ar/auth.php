79cad59eb9e64369967825a00738e8ed3dd4c480
ahmedhabeeb73 <alnaib888@gmail.com>
2021-08-21 15:55:33 +0200

    add ar lang

---
+++ resources/lang/ar/auth.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'البيانات المدخلة غير صحيحة',
+    'password' => 'كلمة السر غير صحيحة',
+    'throttle' => 'أجريت عدد محاولات أكثر من اللازم يرجى المحاولة لاحقا',
+    'Login'=>'الدخول',
+    'Register'=>'تسجيل الدخول',
+    'E-MailAddress'   => 'عنوان البريد الإلكتروني',
+    'Password'        => 'كلمة المرور',
+    'RememberMe'      => ' تذكر بيانات الدخول',
+    'ForgotPassword'  => 'هل نسيت كلمة المرور؟',
+    'Name'            => 'اسم المستخدم',
+    'ConfirmPassword' => 'تأكيد كلمة المرور'
+];
