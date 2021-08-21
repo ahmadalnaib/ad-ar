e9e459ee8c85f1cd0fb24d6b8af022863cfb5803
ahmedhabeeb73 <alnaib888@gmail.com>
2021-08-21 15:37:48 +0200

    add layouts files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title',"رئيسية")
+    
+
+
+@section('content')
+    <h1>الصفحة الرئيسية</h1>
+@endsection
---
+++ resources/views/partials/categoryNav.blade.php
@@ -0,0 +1,7 @@
+<ul class="nav justify-content-center">
+ 
+      <li class="nav-item">
+          <a class="nav-link active" href="">
+      </li>
+
+</ul>
